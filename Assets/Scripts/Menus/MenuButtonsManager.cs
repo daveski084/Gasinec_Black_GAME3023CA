b91d606c3937faa65442a3bba3970731cc90cbeb
David <54949183+daveski084@users.noreply.github.com>
2021-10-06 19:15:16 -0400

    Basic file set up and basic assets

---
+++ Assets/Scripts/Menus/MenuButtonsManager.cs
@@ -0,0 +1,45 @@
+/***********************************************************************;
+* Project            : Untiled
+*
+* Author             : David Gasinec
+* 
+* Student Number     : 101187910
+*
+* Date created       : 21/10/06
+*
+* Description        : Controls button behaviour and scene managment.
+*
+* Last modified      : 21/10/06
+*
+* Revision History   :
+*
+*Date        Author Ref    Revision (Date in YYYYMMDD format) 
+*21/10/06    David Gasinec        Created script. 
+*
+|**********************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class MenuButtonsManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnStartButtonPressed()
+    {
+        Debug.Log("Start Button Pressed!");
+        SceneManager.LoadScene("MainMenu");
+    }
+}
